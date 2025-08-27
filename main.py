1aa1dd2121a4a6b8ef38ae83925c9d80092d7afa
zahar-terekhov <zaharterekhov666@yandex.ru>
2025-08-27 22:14:52 +0900

    asynchronous data archiving method.

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-в
+import asyncio
+from pathlib import Path
 
-def main():
 
-    print("Hello from aiohttp-download-photo!")
+async def create_archive(cmd, size, stdout = b''):
+    process = await asyncio.create_subprocess_exec(
+        *cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+
+    while not process.stdout.at_eof():
+        stdout += await process.stdout.read(size)
+
+    Path("smiles.zip").write_bytes(stdout)
+
+
+async def main():
+    cmd = ["zip", "-r", "-","300_3D_Smiles"]
+    size = 1024*800
+    
+    await asyncio.create_task(create_archive(cmd,size))
 
 
 if __name__ == "__main__":
-    main()
-
+    asyncio.run(main())
