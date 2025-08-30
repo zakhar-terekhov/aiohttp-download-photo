8a89ea9ba2a7e2bc285e881b924b72643e3e64e4
zahar-terekhov <zaharterekhov666@yandex.ru>
2025-08-30 16:12:05 +0900

    split the code into functions, added raising an exception when the directory is missing.

--- server.py
+++ server.py
@@ -6,25 +6,25 @@ import asyncio
 CHUNK_SIZE = 1024 * 800
 
 
-async def prepare_archive_for_downloading(request):
+async def handle_index_page(request):
+    async with aiofiles.open('index.html', mode='r') as index_file:
+        index_contents = await index_file.read()
+    return web.Response(text=index_contents, content_type='text/html')
+
+
+async def prepare_headers(request, archive_name):
     response = web.StreamResponse()
-
-    archive_hash = request.match_info.get('archive_hash')
-
-    archive_name = "archive.part1" if archive_hash == "7kna" else "archive.part2"
 
     response.headers['Content-Type'] = 'application/zip'
     response.headers['Content-Disposition'] = f'attachment; filename="{archive_name}.zip"'
 
     await response.prepare(request)
 
-    return response, archive_hash
+    return response
 
 
-async def download_archive(request):
-    response, archive_hash = await prepare_archive_for_downloading(request)
-
-    photos_dir = Path.cwd().joinpath(f"test_photos/{archive_hash}")
+async def download_archive(request, archive_name, photos_dir):
+    response = await prepare_headers(request, archive_name)
 
     command = ["zip", "-r", "-", "."]
 
@@ -42,16 +42,23 @@ async def download_archive(request):
     return response
 
 
-async def handle_index_page(request):
-    async with aiofiles.open('index.html', mode='r') as index_file:
-        index_contents = await index_file.read()
-    return web.Response(text=index_contents, content_type='text/html')
+async def respond_to_request_download_archive(request):
+    archive_hash = request.match_info.get('archive_hash')
+    archive_name = "archive.part1" if archive_hash == "7kna" else "archive.part2"
+
+    photos_dir = Path.cwd().joinpath(f"test_photos/{archive_hash}")
+
+    if not photos_dir.exists():
+        raise web.HTTPNotFound(text="Архив не существует или был удален")
+
+    response = await download_archive(request, archive_name, photos_dir)
+    return response
 
 
 if __name__ == '__main__':
     app = web.Application()
     app.add_routes([
-        web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', download_archive),
+        web.get('/', handle_index_page, name='index'),
+        web.get('/archive/{archive_hash}/', respond_to_request_download_archive),
     ])
     web.run_app(app)
